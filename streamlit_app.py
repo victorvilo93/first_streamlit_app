6e1d263188255f8cb683ac93654464e6a3ce0ff0
victorvilo93 <75566999+victorvilo93@users.noreply.github.com>
2023-11-15 11:59:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toaste')
 
-streamlit.header('🍌🥭 Prepara tu propio batido de frutas 🥝🍇') 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
